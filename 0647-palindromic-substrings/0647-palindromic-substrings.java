9afc2a49e67c598922f67a775bd87f58afc9272c
Prince Raj <princeraj10d429bdps@gmail.com>
2025-12-24 10:09:01 +0530

    Time: 17 ms (32.26%), Space: 46.7 MB (20.07%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,19 +1,27 @@
 class Solution {
     public int countSubstrings(String s) {
         int n = s.length();
-        int res = 0;
-        for (int i = 0; i < n; i++) {
-            res += fxn(s, i, i);
-            res += fxn(s, i, i + 1);
-        }
-        return res;
-    }
-    int fxn(String s, int i, int j) {
+        boolean[][] dp = new boolean[n][n];
         int count = 0;
-        while (i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)) {
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
             count++;
-            i--;
-            j++;
+        }
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                count++;
+            }
+        }
+        for (int len = 3; len <= n; len++) {
+            for (int i = 0; i + len - 1 < n; i++) {
+                int j = i + len - 1;
+
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    count++;
+                }
+            }
         }
         return count;
     }
