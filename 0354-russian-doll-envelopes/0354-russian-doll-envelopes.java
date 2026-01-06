c2931c69010a73d25f72d649737bf5fce09ba9a4
Prince Raj <princeraj10d429bdps@gmail.com>
2026-01-06 11:51:13 +0530

    Time: 44 ms (55.79%), Space: 101.8 MB (36.17%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.java
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.java
@@ -1,25 +1,30 @@
 class Solution {
     public int maxEnvelopes(int[][] envelopes) {
+        if (envelopes.length == 0) return 0;
         Arrays.sort(envelopes, (a, b) -> {
             if (a[0] == b[0]) return b[1] - a[1]; 
-            return a[0] - b[0]; 
+            return a[0] - b[0];
         });
-        int n = envelopes.length;
-        int[] arra = new int[n];
-        int size = 0;
-        for (int[] env : envelopes) {
-            int h = env[1];
-            int inx = lowerBound(arra, 0, size, h);
-            arra[inx] = h;
-            if (inx == size) size++;
+        ArrayList<Integer> lis = new ArrayList<>();
+        for (int i = 0; i < envelopes.length; i++) {
+            int h = envelopes[i][1];
+            if (lis.isEmpty() || h > lis.get(lis.size() - 1)) {
+                lis.add(h);
+            } else {
+                int idx = lowerBound(lis, h);
+                lis.set(idx, h);
+            }
         }
-        return size;
+        return lis.size();
     }
-    private int lowerBound(int[] arr, int left, int right, int target) {
+    private int lowerBound(ArrayList<Integer> arr, int target) {
+        int left = 0, right = arr.size();
         while (left < right) {
-            int mid = (left + right) / 2;
-            if (arr[mid] < target) left = mid + 1;
-            else right = mid;
+            int mid = left + (right - left) / 2;
+            if (arr.get(mid) < target)
+                left = mid + 1;
+            else
+                right = mid;
         }
         return left;
     }
