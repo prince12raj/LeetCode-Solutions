40df1e7235f6c17bf832adb6edf44aeb5563336d
Prince Raj <princeraj10d429bdps@gmail.com>
2026-02-04 09:31:25 +0530

    Time: 2 ms (99.7%), Space: 53.5 MB (90.67%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,12 +1,15 @@
 class Solution {
     public int findJudge(int N, int[][] trust) {
-        int[] count = new int[N+1];
-        for (int[] t: trust) {
-            count[t[0]]--;
-            count[t[1]]++;
+        int[] indeg = new int[N + 1];
+        int[] outdeg = new int[N + 1];
+        for (int[] t : trust) {
+            outdeg[t[0]]++;
+            indeg[t[1]]++;
         }
-        for (int i = 1; i <= N; ++i) {
-            if (count[i] == N - 1) return i;
+        for (int i = 1; i <= N; i++) {
+            if (indeg[i] == N - 1 && outdeg[i] == 0) {
+                return i;
+            }
         }
         return -1;
     }
