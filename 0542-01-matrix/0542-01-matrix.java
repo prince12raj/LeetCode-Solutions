9a28a7f2c3f8ea21b84cdca0b9f47756c73c82a7
Prince Raj <princeraj10d429bdps@gmail.com>
2026-02-09 12:05:09 +0530

    Time: 20 ms (9.7%), Space: 49 MB (75.87%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -2,11 +2,9 @@ class Solution {
     int n, m;
     int[][] level;
     boolean[][] vis;
-    int[][] mat;
     Queue<int[]> q;
     int[][] move = {{1,0}, {-1,0}, {0,1}, {0,-1}};
     public int[][] updateMatrix(int[][] mat) {
-        this.mat = mat;
         n = mat.length;
         m = mat[0].length;
         level = new int[n][m];
@@ -15,15 +13,17 @@ class Solution {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (mat[i][j] == 0) {
-                    q.offer(new int[]{i, j});
-                    vis[i][j] = true;
                     level[i][j] = 0;
+                    q.offer(new int[]{i, j});
+                } else {
+                    level[i][j] = (int)1e9;
                 }
             }
         }
-        bfs(); 
+        bfs();
         return level;
     }
+
     private void bfs() {
         while (!q.isEmpty()) {
             int[] cur = q.poll();
@@ -32,10 +32,11 @@ class Solution {
             for (int[] d : move) {
                 int nx = x + d[0];
                 int ny = y + d[1];
-                if (nx >= 0 && ny >= 0 && nx < n && ny < m && !vis[nx][ny]) {
-                    vis[nx][ny] = true;
-                    level[nx][ny] = level[x][y] + 1;
-                    q.offer(new int[]{nx, ny});
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+                    if (level[nx][ny] > level[x][y] + 1) {
+                        level[nx][ny] = level[x][y] + 1;
+                        q.offer(new int[]{nx, ny});
+                    }
                 }
             }
         }
