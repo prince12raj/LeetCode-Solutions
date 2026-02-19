7b6df40edac64e44466ac58a17eb4f9c7e3ed71f
Prince Raj <princeraj10d429bdps@gmail.com>
2026-02-19 11:49:46 +0530

    Time: 32 ms (66.95%), Space: 89.5 MB (56.65%) - LeetHub

--- 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.java
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.java
@@ -10,7 +10,7 @@ class Solution {
         int ans = 0;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                ans = (ans + dfs(grid, i, j)) % mod;
+                ans = (ans + dfs(grid, i, j))%mod;
             }
         }
         return ans;
