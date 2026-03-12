4c76b330de13878f0e7e2f7815dbd8e840db776e
Prince Raj <princeraj10d429bdps@gmail.com>
2026-03-12 11:09:56 +0530

    Time: 7 ms (98.78%), Space: 46.6 MB (38.46%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -12,11 +12,15 @@ class Solution {
       if(root == null || root == p || root == q){
         return root;
       }  
+      //left me move karengenge or check karenge root ka left p or q ke equal h ya nahi recursive chalega
       TreeNode left = lowestCommonAncestor(root.left, p ,q);
+      //right me move karengenge or check karenge root ka right p or q ke equal h ya nahi recursive chalega
       TreeNode right = lowestCommonAncestor(root.right, p ,q);
+      //dono ke left null ho to ansestor root hoga
       if(left != null && right !=null){
         return root;
       }
+      // agar left null hua to right return and right null hua  to  return left hoga
       return left !=null ? left : right;
     }
 }
