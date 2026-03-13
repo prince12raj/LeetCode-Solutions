2c1c9a95a11ee66ce33d254e973bf3509a696ca0
Prince Raj <princeraj10d429bdps@gmail.com>
2026-03-13 10:11:17 +0530

    Time: 34 ms (16.03%), Space: 47.3 MB (18.93%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
@@ -4,19 +4,18 @@ class Solution {
             if (a[0] == b[0]) return a[1] - b[1];
             return a[0] - b[0];
         });
-        int n = pairs.length;
-        int[] dp = new int[n];
-        int ans = 1;
-        Arrays.fill(dp, 1);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (pairs[j][1] < pairs[i][0]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+        int n =  pairs.length;
+        int dp[] = new int[n];
+        Arrays.fill(dp , 1);
+        int ans =1;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < i ; j++){
+                if(pairs[j][1] < pairs[i][0]){
+                    dp[i] = Math.max(dp[i] , dp[j] +1);
+                    ans = Math.max(ans ,dp[i]);
                 }
             }
-            ans = Math.max(ans, dp[i]);
         }
-
         return ans;
     }
 }
