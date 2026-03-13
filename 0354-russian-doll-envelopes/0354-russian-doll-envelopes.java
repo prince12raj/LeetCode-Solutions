11a4c82c64780bb4b7a9cf8b44144c9411118dc5
Prince Raj <princeraj10d429bdps@gmail.com>
2026-03-13 09:58:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.java
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.java
@@ -1,31 +1,37 @@
 class Solution {
     public int maxEnvelopes(int[][] envelopes) {
-        if (envelopes.length == 0) return 0;
+        if(envelopes.length == 0) return 0;
+        int n =  envelopes.length;
         Arrays.sort(envelopes, (a, b) -> {
             if (a[0] == b[0]) return b[1] - a[1]; 
             return a[0] - b[0];
         });
-        ArrayList<Integer> lis = new ArrayList<>();
-        for (int i = 0; i < envelopes.length; i++) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i = 0 ; i < n ; i++){
             int h = envelopes[i][1];
-            if (lis.isEmpty() || h > lis.get(lis.size() - 1)) {
-                lis.add(h);
-            } else {
-                int idx = lowerBound(lis, h);
-                lis.set(idx, h);
+            if(arr.isEmpty() || h > arr.get(arr.size() -1)){
+            arr.add(h);
+            }
+            else{
+                int ind = lower(arr , h);
+                arr.set(ind, h);
+
             }
         }
-        return lis.size();
+        return arr.size();
     }
-    private int lowerBound(ArrayList<Integer> arr, int target) {
-        int left = 0, right = arr.size();
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (arr.get(mid) < target)
-                left = mid + 1;
-            else
-                right = mid;
+    private int lower(ArrayList<Integer> arr, int target){
+        int l = 0;
+        int r = arr.size();
+        while(l < r){
+            int mid = (l+r) /2;
+            if(mid > target){
+                l = mid+1;
+            }
+            else {
+                return r =mid;
+            }
         }
-        return left;
+        return l;
     }
 }
