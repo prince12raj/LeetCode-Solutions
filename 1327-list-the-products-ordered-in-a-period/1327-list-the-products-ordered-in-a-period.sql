69555e8d040bb7ece5d68705131566712c613331
Prince Raj <princeraj10d429bdps@gmail.com>
2026-04-01 16:11:16 +0530

    Time: 667 ms (76.57%), Space: 0B (100%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -16,6 +16,6 @@ FROM Products p
 JOIN Orders o
     ON p.product_id = o.product_id
 WHERE EXTRACT(MONTH FROM o.order_date) = 2
-  AND EXTRACT(YEAR FROM o.order_date) = 2020
+  AND EXTRACT(YEAR FROM o.order_date) = 2020.
 GROUP BY p.product_name
 HAVING SUM(o.unit) >= 100;
