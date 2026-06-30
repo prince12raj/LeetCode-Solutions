6bd0dd2a55286e43f0d01e83c23b7c47b8ef13c0
Prince Raj <princeraj10d429bdps@gmail.com>
2026-06-30 17:35:30 +0530

    Time: 2 ms (43.26%), Space: 46.8 MB (98.34%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,42 +1,44 @@
-class Solution {
-    private class DSU{
-        int parent[];
-        int size[];
-        int component;
-        DSU(int n){
-            parent = new int[n];
-            size = new int[n];
-            component = n;
-            for(int i = 0 ; i< n; i++){
-                parent[i] = i;
-                size[i] = 1;
-            }
-        }
-        int find(int x){
-            if(parent[x] == x) return x;
-            return parent[x] = find(parent[x]);
-        }
-        void union(int x ,int y){
-            int px = find(x);
-            int py = find(y);
-            if(px == py) return;
-            if(size[px] < size[py]){
-                parent[px] =py;
-                size[py] += size[px];
-            }else{
-                parent[py] =px;
-                size[px] += size[py];
-            }
-            component--;
+class DSU{
+    int parent[];
+    int size[];
+    int component;
+    DSU(int n){
+        parent = new int[n];
+        size = new int [n];
+        component = n;
+
+        for(int i = 0 ; i < n ; i ++){
+            parent[i] =i;
+            size[i] = i;
         }
     }
+    int find(int x){
+        if(parent[x] == x)return x;
+        return parent[x]= find(parent[x]);
+    }
+    boolean union (int u , int v){
+        int px= find(u);
+        int py = find(v);
+        if(px == py) return false;
+        if(size[px]<size[py]){
+            parent[px] = py;
+            size[py] += size[px];
+        }else{
+            parent[py] = px;
+            size[px] += size[py];
+        }
+        component--;
+        return true;
+    }
+}
+class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
         DSU dsu = new DSU(n);
-        for(int i = 0 ;i < n ; i++){
-            for(int j = 0 ; j< n; j++){
+        for(int i = 0; i < n ; i++){
+            for(int j=0; j<n ; j++){
                 if(isConnected[i][j] == 1){
-                    dsu.union(i ,j);
+                    dsu.union(i, j);
                 }
             }
         }
