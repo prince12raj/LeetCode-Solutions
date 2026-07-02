deaffb1f1f7ee6f604d7e0cd12943571323ef937
Prince Raj <princeraj10d429bdps@gmail.com>
2026-07-02 15:37:31 +0530

    Time: 314 ms (47.05%), Space: 98.7 MB (24.31%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,59 +1,54 @@
 class Solution {
-    class DSU {
-        int[] parent;
-        int[] size;
-        
-        DSU(int n) {
-            parent = new int[n];
-            size = new int[n];
-            for (int i = 0; i < n; i++) {
+    class DSU{
+        int parent [];
+        int size [];
+        DSU(int n){
+            parent = new int [n];
+            size = new int [n];
+            for(int i =0 ; i < n ; i++){
                 parent[i] = i;
-                size[i] = 1;
+                size[i]= 1;
             }
-        } 
-        int find(int i) {
-            if (parent[i] == i) return i;
-            return parent[i] = find(parent[i]);
         }
-        boolean union(int i, int j) {
-            int pi = find(i);
-            int pj = find(j);
-            if (pi == pj) return false;
-            if (size[pi] < size[pj]) {
-                parent[pi] = pj;
-                size[pj] += size[pi];
-            } else {
-                parent[pj] = pi;
-                size[pi] += size[pj];
+        int find(int x){
+            if(parent[x] == x) return x;
+            return parent[x] = find(parent[x]);
+        }
+        boolean union(int x, int y){
+            int px = find(x);
+            int py = find(y);
+            if(px == py) return false ;
+            if(size[px] < size[py]){
+                parent[px] = py;
+                size[py] += size[px];
+            }else{
+                parent[py] = px;
+                size[px] += size[py];
             }
             return true;
         }
     }
     public int minCostConnectPoints(int[][] points) {
         int n = points.length;
-        List<int[]> edges = new ArrayList<>();
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int dist = Math.abs(points[i][0] - points[j][0]) + 
-                           Math.abs(points[i][1] - points[j][1]);
-                edges.add(new int[]{dist, i, j});
+        int m =  points[0].length;
+        ArrayList<int[]> adj = new ArrayList<>();
+        for(int i =0; i < n ; i++){
+            for(int j = i+1 ; j < n ; j++){
+                int dist = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
+                adj.add(new int[]{dist, i ,j});
             }
         }
-        Collections.sort(edges, (a, b) -> a[0] - b[0]);
-        
+        Collections.sort(adj ,(a,b) -> a[0]-b[0]);
         DSU dsu = new DSU(n);
-        int mstCost = 0;
-        int edgesUsed = 0;
-        
-        for (int[] edge : edges) {
-            if (dsu.union(edge[1], edge[2])) {
-                mstCost += edge[0];
-                edgesUsed++;
-                if (edgesUsed == n - 1) break;
+        int usedadge =0;
+        int mstcost =0;
+        for(int e[] : adj){
+            if(dsu.union(e[1], e[2])){
+                mstcost += e[0];
+                usedadge++;
+                if(usedadge == n-1) break;
             }
         }
-        
-        return mstCost;
+        return mstcost;
     }
 }
