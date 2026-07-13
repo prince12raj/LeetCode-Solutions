0f0112ba8904cbebff511e96235d06a859e00704
Prince Raj <princeraj10d429bdps@gmail.com>
2026-07-13 21:06:10 +0530

    Time: 2 ms (31.41%), Space: 47 MB (87.89%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,13 +1,15 @@
 class Solution {
    public int jump(int[] A) {
-	int jumps = 0, curEnd = 0, curFarthest = 0;
-	for (int i = 0; i < A.length - 1; i++) {
-		curFarthest = Math.max(curFarthest, i + A[i]);
-		if (i == curEnd) {
-			jumps++;
-			curEnd = curFarthest;
-		}
-	}
-	return jumps;
-}
+    int jump =0;
+    int curdi  =0;
+    int fur = 0;
+    for(int i = 0; i < A.length -1; i ++){
+        fur = Math.max(fur, i + A[i]);
+        if(i == curdi){
+            jump++;
+            curdi = fur;
+        }
+    }
+    return jump;
+   }
 }
